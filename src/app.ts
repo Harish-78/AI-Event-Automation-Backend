43cd64e62acc70f8d573e4ce6bcedb3151d882e9
Harish-78 <harishc0708@email.com>
2026-01-26 23:26:07 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import dotenv from "dotenv";
 
+dotenv.config();
 import healthRouter from "./routes/health.route";
 import { logger } from "./logger/logger";
+import pool from "./config/db.config";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
-app.use(
-  pinoHttp({
-    logger,
-  }),
-);
+ 
+// connect DB
 
-app.get("/", (req: Request, res: Response) => {
-  req.log.info("something");
+app.get("/", async (req: Request, res: Response) => {
+  const result = await pool.query("WHERE ");
+  logger.info(`Database time: ${result.rows[0].now}`);
   res.send("hello world");
 });
 
